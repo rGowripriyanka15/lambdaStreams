2bc0452f1331d4ac0d8937f6694faaa9b6509ad7
rGowripriyanka15 <60815131+rGowripriyanka15@users.noreply.github.com>
2020-06-26 20:40:43 +0530

    Add files via upload

---
+++ lambdaStreams/src/main/java/EPAM_JAVA_LAMBDA_AND_STREAMS/App.java
@@ -0,0 +1,13 @@
+package EPAM_JAVA_LAMBDA_AND_STREAMS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
